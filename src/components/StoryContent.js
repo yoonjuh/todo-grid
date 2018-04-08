ddbd8ee14037538878e32ab8e82735aebaea3b1a
Juhyung Yoon <ep160101@gmail.com>
2018-04-08 17:32:53 -0400

    Fix structure

---
+++ src/components/StoryContent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const StoryContent = () => <div className="story__content">Story content</div>;
+
+export default StoryContent;
---
+++ src/components/StoryPics.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Story = () => <div className="story__pictures">Story pictures</div>;
+
+export default Story;
