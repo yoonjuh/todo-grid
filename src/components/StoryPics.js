66306653ddd662c03920e5bccdc344064e64ade3
Juhyung Yoon <ep160101@gmail.com>
2018-04-09 08:45:56 -0400

    Add markup for left side of story section

--- src/components/StoryPics.js
+++ src/components/StoryPics.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import img1 from '../style/img/story-1.jpeg';
+import img2 from '../style/img/story-2.jpeg';
 
-const Story = () => <div className="story__pictures">Story pictures</div>;
+const StoryPics = () => (
+  <div className="story__pictures">
+    <img src={img1} alt="Couple with new house" className="story__img--1" />
+    <img src={img2} alt="New house" className="story__img--2" />
+  </div>
+);
 
-export default Story;
+export default StoryPics;
