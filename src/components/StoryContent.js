87cebf7866ce097054afd56675c79233ded35f25
Juhyung Yoon <ep160101@gmail.com>
2018-04-09 07:58:29 -0400

    Add markup for right side of story section

--- src/components/StoryContent.js
+++ src/components/StoryContent.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const StoryContent = () => <div className="story__content">Story content</div>;
-
+const StoryContent = () => (
+  <div className="story__content">
+    <h3 className="heading-3 mb-sm">Happy Customers</h3>
+    <h2 className="heading-2 heading-2--dark mb-md">&ldquo;The best choice of our lives&rdquo;</h2>
+    <p className="story__text">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.
+      Quidem consequatur harum volupta!
+    </p>
+    <button className="btn">Find your own home</button>
+  </div>
+);
 export default StoryContent;
