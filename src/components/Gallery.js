5676f31cbf08eae4aedbc0e336829d5c26aad7ba
Juhyung Yoon <ep160101@gmail.com>
2018-04-12 22:37:34 -0400

    Add basic of Gallery section

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import img1 from '../style/img/gal-1.jpeg';
 
-const Gallery = () => <section className="gallery">Gallery</section>;
+const Gallery = () => (
+  <section className="gallery">
+    <img src={img1} alt="Gallery image1" className="gallery__img" />
+  </section>
+);
 
 export default Gallery;
