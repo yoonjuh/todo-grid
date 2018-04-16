f92d38ba6cc27179704a5f8acb25c5962a76e8f4
Juhyung Yoon <ep160101@gmail.com>
2018-04-16 06:44:57 -0400

    Add footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const Footer = () => <footer className="footer">Footer</footer>;
+const Footer = () => (
+  <footer className="footer">
+    <ul className="nav">
+      <li className="nav__item"><a href="#" className="nav__link">Fine your dream home</a></li>
+      <li className="nav__item"><a href="#" className="nav__link">Request personal</a></li>
+      <li className="nav__item"><a href="#" className="nav__link">Download home planner</a></li>
+      <li className="nav__item"><a href="#" className="nav__link">Contact us</a></li>
+      <li className="nav__item"><a href="#" className="nav__link">Submit your property</a></li>
+      <li className="nav__item"><a href="#" className="nav__link">Come work with us</a></li>
+    </ul>
+    <p className="copyright">
+      &copy; Copyright 2017 by Jonas Schmedtmann. Feel free to use this project for your own purposes. This does not apply if you plan to produce your own course or tutorials based onn this project.
+    </p>
+  </footer>
+
+);
 
 export default Footer;
